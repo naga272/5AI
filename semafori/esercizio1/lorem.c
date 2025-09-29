4cf28f005b28ae9b2b0d6f62e1884cfc1e89ff70
naga272 <bastianellofederico4@gmail.com>
2025-09-29 17:52:08 +0200

    Aggiunti esercizi c sui semafori

---
+++ semafori/esercizio1/lorem.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+
+
+int main(int argc, char **argv, char **envp)
+{
+    key_t key = 1234;
+
+    int fdsem = semget(key, 1, IPC_CREAT | 0666);
+
+    if (fdsem == -1) {
+        printf("error fd sem");
+        exit(EXIT_FAILURE);
+    }
+
+    struct sembuf sb = {
+        0,      // semaforo numero ...
+        1,      // aumenta
+        0       // flags
+    };
+
+    semop(fdsem, &sb, 1);
+
+    return EXIT_SUCCESS;
+}
