c4b5cb1e02e2517e23f49cdf39281f5f66100d03
Nagato272 <114398392+naga272@users.noreply.github.com>
2025-10-30 13:44:45 +0100

    Update curva_domanda.py

--- curva_domanda.py
+++ curva_domanda.py
@@ -1,17 +1,35 @@
+import matplotlib as mpt 
+import matplotlib.pyplot as plt 
+import numpy as np
+import pandas as pd
+
 # Quantita di un determinato bene che il consumatore e' disposto ad acquistare
 # q = 80 - (10/3)*p
 # dove p è il prezzo di 1 Gb
 # Se vende 70 gb di spazio alla settimana?
 # p = (80 - (10/3)) / q
 
-  
+
 def domanda(q: int) -> float:
     return (80 - q) / (10 / 3)
 
 
-all_quantita = [
-    10, 20, 30, 40, 50, 60, 70
-]
+all_prezzo = []
+all_quantita = [10, 20, 30, 40, 50, 60, 70]
 
-for quantita in all_quantita:
-    print(domanda(quantita))
+
+for quantita in (all_quantita):
+    all_prezzo.append(domanda(quantita) * quantita)
+
+
+x = np.array(all_quantita)
+y = np.array(all_prezzo)
+
+plt.plot(x, y)
+
+plt.xlabel("quantita")
+plt.ylabel("Prezzo")
+plt.title("Grafico della curva")
+plt.savefig("./figura.png")
+plt.show()
+
